60cdd9ca00867402712243e71c7888014d1dc3b7
repl.it user <replituser@example.com>
2020-10-23 13:32:55 +0000

    changed version

--- Zahlenfilter.java
+++ Zahlenfilter.java
@@ -5,9 +5,9 @@ class Main {
     System.out.println("Zahl ist durch 5 teilbar!" +i);}
      if( i%10==9 ){
     System.out.println("Zahl endet auf 9!" +i);}
-    if(i!=0){
+   
     if(((2*i-1) % 3)==0){
        System.out.println("ZAHL und VORGÄNGER addiert ergeben "+ i +" und "+(i-1) +"ist durch 3 teilbar");}}
-    }
+    
   }
 }
